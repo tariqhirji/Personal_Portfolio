ef845279aa755982731e1620608010e8f121ed3c
Tariq <tariqhirji15@gmail.com>
2019-06-14 19:05:28 -0400

    Fixed layout of css and html page. Added Scrolling navbar animation
    JS function that scrolls with the body. Changes colour and respective section of the page.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,55 @@
+// Function that changes navbar colour at different points of the page. Also highlights section colour.
+function scroll() {
+    
+    // Get scroll length and navbar variables
+    var t = window.scrollY;
+    var nav = document.getElementById("navbar");
+    var navigation = document.getElementById("navigation");
 
+    // variables to hold links of navbar.
+    var brand = document.getElementById("brand");
+    var l1 = document.getElementById("skills-link");
+    var l2 = document.getElementById("project-link");
+    var l3 = document.getElementById("about-link");
+    var l4 = document.getElementById("contact-link");
 
+    // variables to hold sections of the page.
+    var s1 = document.getElementById("skills-section");
+    var s2 = document.getElementById("projects-section");
+    var s3 = document.getElementById("about-section");
+    var s4 = document.getElementById("contact-section");
 
-var effects = 'animated bounce';
-var effectsEnd = 'animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd';
+    // variables to hold offset from sections to the top of the page.
+    var t1 = s1.offsetTop;
+    var t2 = s2.offsetTop;
+    var t3 = s3.offsetTop;
+    var t4 = s4.offsetTop;
 
-// Get social icon image to bounce on hover by adding classes and removing
-    $(".social-icon").mouseover(function() {
-        $(this).addClass(effects).one(effectsEnd, function () {
-            $(this).removeClass(effects);
-        });
-    });
+    //hold colours of original links to change back to after leaving section
+    l1.style.color = "white";
+    l2.style.color = "white";
+    l3.style.color = "white";
+    l4.style.color = "white";
+    brand.style.color = "white";
+
+    // Conditional that checks which section of the page the user is currently on
+    if (t > t4 - 50) {
+        l4.style.color = "black";
+    } else if (t > t3 - 50) {
+        l3.style.color = "black";
+        nav.style.backgroundColor = "transparent";
+        navigation.style.backgroundColor = "transparent";
+    } else if (t > t2 - 50) {
+        l2.style.color = "black";
+        nav.style.backgroundColor = "#797979";
+        navigation.style.backgroundColor = "#797979";
+    } else if (t > t1 - 50) {
+        l1.style.color = "black";
+        nav.style.backgroundColor = "deepskyblue";
+        navigation.style.backgroundColor = "deepskyblue";
+    } else {
+        brand.style.color = "black";
+        nav.style.backgroundColor = "lightskyblue";
+        navigation.style.backgroundColor = "lightskyblue";
+    }
+}
